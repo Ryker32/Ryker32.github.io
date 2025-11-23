472c29844c2fa78c768feea4dfe9756a34bf75ef
Ryker <Rykerkollmyer@gmail.com>
2025-11-22 20:24:26 -0800

    reip size

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -202,7 +202,7 @@
       The agents each have a local view of the environment and a shared "Simultaneous Localization And Mapping" (SLAM) view of the environment through a custom communication protocol.
       The simulation can be ran under .YAML configuration files that define which agent policy is to be ran within the environment for comparison and ablation tests.
       </p>
-      <figure class="project-figure project-figure--left object-cover reip-figure">
+      <figure class="project-figure project-figure--left object-cover" style="height: 400px; max-height: 400px; width: 400px; max-width: 400px;">
         <img src="/assets/img/reip/reipsquare.png" alt="REIP Simulation Demo">
         <figcaption>Demo image of the REIP simulation environment.</figcaption>
       </figure>
